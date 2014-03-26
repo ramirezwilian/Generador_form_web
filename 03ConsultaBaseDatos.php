0119354a27e7a9559978541a677fe3aa7d040816
Erik Daniel García Aquino <eAquino@users.noreply.github.com>
2014-03-26 11:30:35 -0600

    Update 03ConsultaBaseDatos.php

--- 03ConsultaBaseDatos.php
+++ 03ConsultaBaseDatos.php
@@ -1,74 +1,41 @@
-<!DOCTYPE HTML>
-	<head>
-		<title> Pagina dos, Registro de jugadores </title>
-	</head>
-	<body>
-		
-	<!-- ........... Conexion a la base de datos ...........-->
-	<?php
+<?php
 
+	function GuardarDatos($cadena,$field)
+	{
 		// tiposDeDatos guardara el Type de dato de mysql
 		$tiposDeDatos = array();
 		$inputType = "string";
-		
+
 		// campoDeTabla guardara el Field de la tabla de mysql
 		$campoDeTabla = array();
 		$labelName = "string";
 
-		// ValorTabla guarda 
+		$tipo = substr( $cadena, 0, -4);
+		$tiposDeDatos[]=$tipo;
+		array_push();
 
+		$campoDeTabla[]= $field;
+		array_push();
 
-		require_once("comparacion.php");
-		
-		$conexion=mysql_connect("localhost","root","Linces25z")
-		 or die("Problemas en la conexion");
-		 
-		 mysql_select_db("base1", $conexion) 
-		 or die("Problemas en la seleccion de base de datos");
-		 
-		 // Desc de la tabla 
-		 $desc=mysql_query("describe jugador", $conexion)
-		 or die("Problemas en el insert".mysql_error() );
+		for($i=0; $i < count($tiposDeDatos); $i++)
+		{
+			switch($tiposDeDatos[$i])
+			{
+				case "tinyint" : $inputType = "checkbox"; 
 
-		 // Resultados de la primera tabla, Describe table
-		 while($row=mysql_fetch_array($desc))
-		 {
-			echo " {$row['Field']} --- {$row['Type']} ";
-			//$tipo = substr( $row['Type'], 0, -4);
-			//$tiposDeDatos[]=$tipo;
-			//array_push();
-			 
-			//$campoDeTabla[]= $row['Field'];
-			//array_push();
+							break;	
+				case "varchar" : $inputType = "text"; 
 
-			$tipo = substr( $row['Type'], 0, -4);
-			$campo = $row['Field'];
-			// Consulta de la tabla
+							break;
+				case "date" : $inputType = "password"; 
 
-		 	$select=mysql_query("select ".$row['Type']." from jugador", $conexion)
-		 	 or die("Problemas en el insert".mysql_error() );
+							break;
+				case "int" : $inputType = "tel"; 
 
-			CompararTipodeDato($tipo,$campo);
-
-		 }
-
-		echo "<br>";
-
-		CompararDatos($tiposDeDatos,$campoDeTabla);
-		
-		// Resultados de la segunda tabla. SELECT  * FROM TABLE 
-		echo "<table border='1'>";
-		 while($reg=mysql_fetch_array($select))
-		 {
-			echo "<tr>";
-			echo "<td> ".$reg['."$".']." </td>";
-			echo "</tr>";
-		 }
-		echo "</table>"; 
-		 
-		mysql_close($conexion);
-		echo "<br> esto es lo que hay en las tablas de la base de datos ";
-	?>
-	</body>
-	<a href="index.html"> Regresar </a>
-</HTML>
+							break;
+			}
+			echo "<h3>".$campoDeTabla[$i]."</h3>";
+			echo "<input type=".$inputType." name=txt".$campoDeTabla[$i]."> <br>";
+		}
+	}
+?>
